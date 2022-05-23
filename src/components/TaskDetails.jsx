f4186ad6d9ce19bd729c90f0bc90e0b66b9e0bd7
phucnq <phuc.nguyen@mor.com.vn>
2022-05-23 07:54:38 +0700

    add react-input

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -3,20 +3,39 @@ import AsyncSelect from "react-select/async";
 
 const TaskDetails = () => {
   const colourOptions = [
-    "blue",
-    "green",
-    "yellow",
-    "orange",
-    "yellow",
-    "red",
-    "black",
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
   ];
+
+  const customStyles = {
+    control: (base, state) => ({
+      ...base,
+      // Overwrittes the different states of border
+      borderColor: state.isFocused ? "yellow" : "green",
+      // Removes weird border around container
+      boxShadow: state.isFocused ? null : null,
+      "&:hover": {
+        // Overwrittes the different states of border
+        borderColor: state.isFocused ? "red" : "blue",
+      },
+      width: 270,
+      height: 46,
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: "1px dotted pink",
+      color: state.isSelected ? "red" : "blue",
+      padding: 20,
+      width: 270,
+    }),
+  };
 
   const [inputValue, setInputValue] = useState("");
 
   const filterColors = (inputValue) => {
     return colourOptions.filter((i) =>
-      i.toLowerCase().includes(inputValue.toLowerCase())
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
     );
   };
 
@@ -28,7 +47,6 @@ const TaskDetails = () => {
 
   const handleInputChange = (newValue) => {
     const inputValue = newValue.replace(/\W/g, "");
-    setInputValue(inputValue);
     return inputValue;
   };
 
@@ -41,12 +59,14 @@ const TaskDetails = () => {
       <div className="task-detail__content">
         <h3>Thông tin KR</h3>
         <p>Chọn KR</p>
-        <pre>inputValue: "{inputValue}"</pre>
         <AsyncSelect
+          classNamePrefix="react-select"
+          styles={customStyles}
           cacheOptions
           loadOptions={loadOptions}
           defaultOptions
           onInputChange={handleInputChange}
+          placeholder={"-"}
         />
       </div>
     </div>
